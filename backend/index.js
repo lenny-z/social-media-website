18b92409796134afc828a19919420dc8ad47c23f
lenny-z <zhoulangchun@gmail.com>
2023-02-26 16:32:45 -0500

    Granted permissions on DB to API

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const { Client } = require('pg');
 app.use(cors());
 app.use(express.json());
 
-const credentials = {
+const CREDENTIALS = {
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
@@ -22,17 +22,16 @@ function testConnectDB(credentials) {
         .then(() => console.log('DB test connection successful'))
         .catch((err) => console.error(err.stack));
 
-    const query = 'SELECT $1::text as message';
+    const query = 'SELECT $1::text as message;';
     const params = ['DB test query successful'];
 
     client.query(query, params)
         .then((res) => console.log(res.rows[0].message))
         .catch((err) => console.error(err.stack))
         .then(() => client.end());
-
 }
 
-testConnectDB(credentials);
+testConnectDB(CREDENTIALS);
 
 function prettyJSON(jsonObj) {
     return JSON.stringify(jsonObj, null, 4);
@@ -41,15 +40,6 @@ function prettyJSON(jsonObj) {
 //one connect/disconnect per query
 //to avoid injection attacks, don't directly concatenate parameters to query
 //instead, use parameterized queries
-
-// function queryDB(credentials, myQuery, myParams, backendResponse) {
-//     const client = new Client(credentials);
-//     client.connect();
-//     client.query(myQuery, myParams)
-//         .then((res) => backendResponse(res))
-//         .catch((err) => console.error(err.stack))
-//         .then(() => client.end());
-// }
 
 app.post('/login', (req, res) => {
     console.log('POST to /login:');
@@ -63,9 +53,9 @@ const USERNAME_COLUMN = process.env.USERNAME_COLUMN;
 console.log(USERS_TABLE);
 
 async function userExists(username) {
-    const query = `SELECT ${USERNAME_COLUMN} FROM ${USERS_TABLE} WHERE ${USERNAME_COLUMN} = $1`;
+    const query = `SELECT ${USERNAME_COLUMN} FROM ${USERS_TABLE} WHERE ${USERNAME_COLUMN} = $1;`;
     const params = [username];
-    const client = new Client(credentials);
+    const client = new Client(CREDENTIALS);
     client.connect();
     const res = await client.query(query, params);
     client.end();
@@ -81,38 +71,32 @@ app.post('/register', async (req, res) => {
     console.log('POST to /register:');
     console.log(prettyJSON(req.body));
     console.log();
-    // console.log(await userExists(req.body.username));
 
     if (await userExists(req.body.username)) {
         res.status(200).json({ error: 'Username already exists' });
     } else {
-        const query = `BEGIN; INSERT INTO ${USERS_TABLE}(${USERNAME_COLUMN}) VALUES ($1); COMMIT;`;
+        // const query = `BEGIN; INSERT INTO ${USERS_TABLE}(${USERNAME_COLUMN}) VALUES ($1); COMMIT;`;
         // const password_hash_salt = await argon2.hash(req.body.password);
         // const params = [req.body.username, password_hash_salt];
-        const params = [req.body.username];
-        const client = new Client(credentials);
-        client.connect();
-        await client.query(query, params);
-        client.end();
+        // const params = [req.body.username];
+        // const client = new Client(CREDENTIALS);
+        const client = new Client(CREDENTIALS);
+        await client.connect();
+        try {
+            await client.query('BEGIN;');
+            var query = `INSERT INTO ${USERS_TABLE}(${USERNAME_COLUMN}) VALUES ($1);`;
+            var params = [req.body.username];
+            await client.query(query, params);
+            await client.query('COMMIT;');
+        } catch (err) {
+            await client.query('ROLLBACK;');
+            console.error(err);
+        } finally {
+            client.end();
+        }
     }
-
-    // const query = 'SELECT email FROM users WHERE email = $1';
-    // const params = [req.body.username];
-
-    // const backendResponse = function (dbResponse) {
-    //     console.log(prettyJSON(dbResponse));
-    // }
-
-    // queryDB(credentials, query, params, backendResponse)
-    // console.log(userExists(req.body.username));
-    // await userExists(req.body.username);
-    // try {
-    //     const ye = await userExists(req.body.username);
-    //     console.log(await userExists(req.body.username));
-    // }catch{}
 });
 
-// require('dotenv').config();
 const port = process.env.PORT;
 
 app.listen(port, () => {
