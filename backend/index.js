14871bc510f4860fa1f33187e52728452bdbed0f
lenny-z <zhoulangchun@gmail.com>
2023-03-12 20:00:46 -0400

    Stored password hashes

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ function testConnectDB(pool) {
     const params = ['DB test query successful'];
 
     pool.query(query, params)
-        .then((res) => console.log(res.rows[0].message))
+        .then((res) => console.log(res.rows[0].message + '\n'))
         .catch((err) => console.error(err.stack));
 }
 
@@ -34,29 +34,29 @@ testConnectDB(pool);
 function prettyJSON(jsonObj) {
     const out = JSON.stringify(jsonObj, null, 4);
 
-    if (out.length > 256) {
-        return `${out.substring(0, 256)} ...`;
+    if (out.length > 512) {
+        return `${out.substring(0, 512)} ...`;
     }
 
-    // return JSON.stringify(jsonObj, null, 4).substring(0, 256);
+    out += '\n';
     return out;
 }
 
 //Don't log passwords
+
 app.post('/login', (req, res) => {
-    // console.log('POST to /login:');
-    // console.log(prettyJSON(req.body));
-    // console.log();
     res.send('yo');
 });
 
 const USERS_TABLE = process.env.USERS_TABLE;
+const ID_COLUMN = process.env.ID_COLUMN; //Assumes that all serial primary key columns share the same name
 const EMAIL_COLUMN = process.env.EMAIL_COLUMN;
 
 async function getUser(username, pool) {
     console.log(`getUser(${username}, pool):`);
 
-    const query = `SELECT ${EMAIL_COLUMN} FROM ${USERS_TABLE} WHERE ${EMAIL_COLUMN} = $1;`;
+    // const query = `SELECT ${ID_COLUMN}, ${EMAIL_COLUMN} FROM ${USERS_TABLE} WHERE ${EMAIL_COLUMN} = $1;`;
+    const query = `SELECT ${ID_COLUMN} FROM ${USERS_TABLE} WHERE ${EMAIL_COLUMN} = $1;`;
     console.log(`query: ${query}`)
 
     const params = [username];
@@ -69,44 +69,38 @@ async function getUser(username, pool) {
 }
 
 //https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
-//By default, argon2id hash stores its own salt
+//By default, argon2id digest stores its own salt
 const argon2 = require('argon2');
 const SALTED_PASSWORD_HASHES_TABLE = process.env.SALTED_PASSWORD_HASHES_TABLE;
 const SALTED_PASSWORD_HASH_COLUMN = process.env.SALTED_PASSWORD_HASH_COLUMN;
+const USER_ID_COLUMN = process.env.USER_ID_COLUMN;
 
 app.post('/register', async (req, res) => {
     console.log('POST to /register:');
-    // console.log(`req.body: ${prettyJSON(req.body)}`);
     const user = await getUser(req.body.username, pool);
     console.log(prettyJSON(user));
 
     if (user.rowCount > 0) {
         res.sendStatus(409);
     } else {
-        // const query = `BEGIN; INSERT INTO ${USERS_TABLE}(${USERNAME_COLUMN}) VALUES ($1); COMMIT;`;
-        // const password_hash_salt = await argon2.hash(req.body.password);
-        // const params = [req.body.username, password_hash_salt];
-        // const params = [req.body.username];
-        // const client = new Client(CREDENTIALS);
-        // const client = new Client(CREDENTIALS);
-        // await client.connect();
         const client = await pool.connect();
         try {
             await client.query('BEGIN;');
             var query = `INSERT INTO ${USERS_TABLE}(${EMAIL_COLUMN}) VALUES ($1);`;
             var params = [req.body.username];
             await client.query(query, params);
-            // const salted_password_hash = await argon2.hash(req.body.password);
-            // query = `INSERT INTO ${SALTED_PASSWORD_HASHES_TABLE}(${USER_ID_COLUMN}, ${SALTED_PASSWORD_HASH_COLUMN})
-            // VALUES(${user.})`
-
+            const salted_password_hash = await argon2.hash(req.body.password);
+            // query = `INSERT INTO ${SALTED_PASSWORD_HASHES_TABLE}(${USER_ID_COLUMN}, ${SALTED_PASSWORD_HASH_COLUMN}) VALUES(${user.id}, ${salted_password_hash});`;
+            query = `INSERT INTO ${SALTED_PASSWORD_HASHES_TABLE}(${USER_ID_COLUMN}, ${SALTED_PASSWORD_HASH_COLUMN}) VALUES
+            ((SELECT ${ID_COLUMN} FROM ${USERS_TABLE} WHERE ${EMAIL_COLUMN} = $1), $2);`;
+            params = [req.body.username, salted_password_hash];
+            await client.query(query, params);
             // query = `INSERT INTO ${SALTED_PASSWORD_HASHES_TABLE}()`;
             await client.query('COMMIT;');
         } catch (err) {
             await client.query('ROLLBACK;');
             console.error(err);
         } finally {
-            // client.end();
             client.release();
         }
     }
@@ -115,6 +109,5 @@ app.post('/register', async (req, res) => {
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    // console.log('Server running on port ' + port);
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}\n`);
 });
--- setup.sql
+++ setup.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id          SERIAL PRIMARY KEY,
     email       VARCHAR(254) UNIQUE NOT NULL
-
-    --username    VARCHAR(320)
 );
 
 CREATE TABLE salted_password_hashes (
@@ -13,17 +11,13 @@ CREATE TABLE salted_password_hashes (
     user_id                 INT REFERENCES users (id) ON DELETE CASCADE,
     --argon2id
     salted_password_hash    VARCHAR NOT NULL
-    -- CONSTRAINT fk_user_id
-    --     FOREIGN KEY(user_id)
-    --         REFERENCES users(id)
 );
 
--- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM social_network_backend;
 REASSIGN OWNED BY social_network_backend TO postgres;
 DROP OWNED BY social_network_backend;
 DROP ROLE IF EXISTS social_network_backend;
 CREATE ROLE social_network_backend;
 --Manually grant login and password to avoid exposing key in setup.sql to Git
--- GRANT USAGE ON SCHEMA public TO social_network_backend;
 GRANT SELECT, INSERT, UPDATE, DELETE ON users, salted_password_hashes TO social_network_backend;
 GRANT USAGE, SELECT ON SEQUENCE users_id_seq TO social_network_backend;
+GRANT USAGE, SELECT ON SEQUENCE salted_password_hashes_id_seq TO social_network_backend;
