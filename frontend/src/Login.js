ca1be13dbd08573521a99e88f2c22706f886f3b0
lenny-z <zhoulangchun@gmail.com>
2023-03-17 04:04:30 -0400

    Converted Login.js to hooks

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,68 +1,128 @@
-import React from 'react';
+// import React from 'react';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+
+// export default class Login extends React.Component {
+//     constructor(props) {
+//         super(props);
+
+//         this.onSubmit = this.onSubmit.bind(this);
+//         this.onChangeUsername = this.onChangeUsername.bind(this);
+//         this.onChangePassword = this.onChangePassword.bind(this);
+
+//         this.state = {
+//             username: '',
+//             password: ''
+//         };
+//     }
+
+//     onSubmit(event) {
+//         event.preventDefault();
+
+//         const user = {
+//             username: this.state.username,
+//             password: this.state.password
+//         };
+
+//         axios.post(process.env.REACT_APP_LOGIN, user)
+//             .then(res => console.log(res.data));
+//     }
+
+//     onChangeUsername(event) {
+//         this.setState({
+//             username: event.target.value
+//         });
+//     }
+
+//     onChangePassword(event) {
+//         this.setState({
+//             password: event.target.value
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <form onSubmit={this.onSubmit}>
+//                 <label htmlFor='username'>Username: </label>
+//                 <input
+//                     id='username'
+//                     type='text'
+//                     value={this.state.username}
+//                     onChange={this.onChangeUsername}
+//                 />
+//                 <label htmlFor='password'>Password: </label>
+//                 <input
+//                     id='password'
+//                     type='password'
+//                     value={this.state.password}
+//                     onChange={this.onChangePassword}
+//                 />
+//                 <input
+//                     type='submit'
+//                     value='Log In'
+//                 />
+//                 <Link to={'register'}>Register</Link>
+//             </form>
+//         );
+//     }
+// }
+
+import { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
-export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
+// const util = require('./util.js');
 
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChangeUsername = this.onChangeUsername.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-        this.state = {
-            username: '',
-            password: ''
-        };
-    }
-
-    onSubmit(event) {
+    async function handleSubmit(event) {
         event.preventDefault();
 
         const user = {
-            username: this.state.username,
-            password: this.state.password
+            username: username,
+            password: password
         };
 
-        axios.post(process.env.REACT_APP_LOGIN, user)
-            .then(res => console.log(res.data));
+        const res = await axios.post(process.env.REACT_APP_LOGIN, user);
+        console.log(res.data);
+
+        if (res.status === 201) {
+            navigate('/home');
+        }
     }
 
-    onChangeUsername(event) {
-        this.setState({
-            username: event.target.value
-        });
+    function handleUsername(event) {
+        setUsername(event.target.value);
     }
 
-    onChangePassword(event) {
-        this.setState({
-            password: event.target.value
-        });
+    function handlePassword(event) {
+        setPassword(event.target.value);
     }
 
-    render() {
-        return (
-            <form onSubmit={this.onSubmit}>
-                <label htmlFor='username'>Username: </label>
-                <input
-                    id='username'
-                    type='text'
-                    value={this.state.username}
-                    onChange={this.onChangeUsername}
-                />
-                <label htmlFor='password'>Password: </label>
-                <input
-                    id='password'
-                    type='password'
-                    value={this.state.password}
-                    onChange={this.onChangePassword}
-                />
-                <input
-                    type='submit'
-                    value='Log In'
-                />
-                <Link to={'register'}>Register</Link>
-            </form>
-        );
-    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username: </label>
+            <input
+                id='username'
+                type='text'
+                value={username}
+                onChange={handleUsername}
+            />
+            <label htmlFor='password'>Password: </label>
+            <input
+                id='password'
+                type='password'
+                value={password}
+                onChange={handlePassword}
+            />
+            <input
+                type='submit'
+                value='Log In'
+            />
+            <Link to={'register'}>Register</Link>
+        </form>
+    );
 }
