a71b4fbd0501647e20cb16864163b58b7975ae7e
lenny-z <zhoulangchun@gmail.com>
2023-04-09 13:17:08 -0400

    Moved profileLoader from index to Profile

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,15 +1,36 @@
 import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
 import ContentHeader from './ContentHeader.js';
 import PostsList from './PostsList.js';
 
+export async function loader({ params }) {
+	console.log('Profile.loader:');
+	const data = {};
+	data.contentHeader = params.username;
+
+	try {
+		const res = await axios.get(
+			`${process.env.REACT_APP_PROFILE_POSTS}/${params.username}`
+		);
+
+		if (res.status === 200) {
+			data.posts = res.data;
+		}
+	} catch (err) {
+		console.log(err);
+		data.posts = null;
+	}
+
+	return data;
+}
+
 export default function Profile() {
 	const data = useLoaderData();
-	// const contentHeader = data.contentHeader;
-	// const posts = data.posts;
 
 	return (
 		<>
 			<ContentHeader contentHeader={data.contentHeader} />
+
 			<PostsList posts={data.posts} />
 		</>
 	);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,29 +9,29 @@ import Login from './Login.js';
 import Register from './Register.js';
 import Home from './Home.js';
 import Search from './Search.js';
-import Profile from './Profile.js';
+import Profile, {loader as profileLoader} from './Profile.js';
 import Settings from './Settings.js';
 
-async function profileLoader({ params }) {
-	console.log('index.profileLoader:');
-	const data = {};
-	data.contentHeader = params.username;
+// async function profileLoader({ params }) {
+// 	console.log('index.profileLoader:');
+// 	const data = {};
+// 	data.contentHeader = params.username;
 
-	try {
-		const res = await axios.get(
-			`${process.env.REACT_APP_PROFILE_POSTS}/${params.username}`
-		);
+// 	try {
+// 		const res = await axios.get(
+// 			`${process.env.REACT_APP_PROFILE_POSTS}/${params.username}`
+// 		);
 
-		if (res.status === 200) {
-			data.posts = res.data;
-		}
-	} catch (err) {
-		console.log(err);
-		data.posts = null;
-	}
+// 		if (res.status === 200) {
+// 			data.posts = res.data;
+// 		}
+// 	} catch (err) {
+// 		console.log(err);
+// 		data.posts = null;
+// 	}
 
-	return data;
-}
+// 	return data;
+// }
 
 const router = createBrowserRouter([
 	{
