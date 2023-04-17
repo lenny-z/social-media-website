10b2bca6e856f822a6e56d08f9cc1c186c9f578a
lenny-z <zhoulangchun@gmail.com>
2023-04-17 15:34:08 -0400

    Refactoring post names

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -5,11 +5,9 @@ export default function Post({ poster, post, timePosted }) {
 	return (
 		<div className='post'>
 			<Link className='username-link'>{poster}</Link>
-			{/* <br /> */}
 			<div className='time-posted-div'>
 				{timePosted}
 			</div>
-			{/* <br /> */}
 			<div className='post-div'>
 				{post}
 			</div>
