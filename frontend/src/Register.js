65507f86c440ec50f141b11c8e0244b7fb95fe38
lenny-z <zhoulangchun@gmail.com>
2023-05-03 18:45:54 -0400

    Making validation package

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import { useNavigate, useOutletContext, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import axios from 'axios';
 import ContentHeader from './ContentHeader.js';
 import ContentBody from './ContentBody.js';
 import './css/Register.css';
 
-const EMAIL_REGEX = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+// const EMAIL_REGEX = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+const validator = require('@lenny_zhou/validator');
+// const emailValidator = validator.email;
 
 export default function Register({
 	isAuthorized,
@@ -15,9 +17,6 @@ export default function Register({
 	const [email, setEmail] = useState('');
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
-	// const setAuthorized = useOutletContext()[1];
-	// const setReturnedUsername = useOutletContext()[3];
-	// const navigate = useNavigate();
 
 	async function handleSubmit(event) {
 		event.preventDefault();
@@ -35,7 +34,6 @@ export default function Register({
 			if (res.status === 201) {
 				setAuthorized(true);
 				setReturnedUsername(res.data);
-				// navigate('/');
 			}
 		} catch (err) {
 			if (err.response && err.response.status === 500) {
@@ -50,6 +48,7 @@ export default function Register({
 
 	function handleEmail(event) {
 		setEmail(event.target.value);
+		// console.log(validator.email(email))
 	}
 
 	function handlePassword(event) {
