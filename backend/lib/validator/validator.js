923ed08531ec168ef09b42ab673e6ee626a44064
lenny-z <zhoulangchun@gmail.com>
2023-05-04 00:43:31 -0400

    Preparing to refactor registration flow

--- backend/lib/validator/validator.js
+++ backend/lib/validator/validator.js
@@ -1,9 +1,59 @@
-const emailRegex = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const usernameCharsRegex = /^[a-zA-Z0-9_-]$/;
 
 exports.email = (email) => {
 	return emailRegex.test(email);
 };
 
-// exports.username = (username)=>{
+exports.username = (username) => {
+	const requirements = {
+		isString: false,
+		lengthAtLeast1: false,
+		lengthAtMost32: false,
+		allowedCharactersOnly: false
+	}
 
-// }
+	if (typeof username === 'string') {
+		requirements.isString = true;
+	} else {
+		return requirements;
+	}
+
+	if (username.length >= 1) {
+		requirements.lengthAtLeast1 = true;
+	}
+
+	if (username.length <= 32) {
+		requirements.lengthAtMost32 = true;
+	}
+
+	if (usernameCharsRegex.test(username) === true) {
+		requirements.allowedCharactersOnly = true;
+	}
+
+	return requirements;
+};
+
+exports.password = (password) => {
+	const requirements = {
+		isString: false,
+		lengthAtLeast14: false,
+		lengthAtMost128: false,
+	}
+
+	if (typeof password === 'string') {
+		requirements.isString = true;
+	} else {
+		return requirements;
+	}
+
+	if (username.length >= 14) {
+		requirements.lengthAtLeast14 = true;
+	}
+
+	if (username.length <= 128) {
+		requirements.lengthAtMost128 = true;
+	}
+
+	return requirements;
+}
--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -5,9 +5,7 @@ import ContentHeader from './ContentHeader.js';
 import ContentBody from './ContentBody.js';
 import './css/Register.css';
 
-// const EMAIL_REGEX = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
 const validator = require('@lenny_zhou/validator');
-// const emailValidator = validator.email;
 
 export default function Register({
 	isAuthorized,
@@ -17,6 +15,8 @@ export default function Register({
 	const [email, setEmail] = useState('');
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+
+	// const emailIsValid = validator.email(email);
 
 	async function handleSubmit(event) {
 		event.preventDefault();
@@ -44,11 +44,12 @@ export default function Register({
 
 	function handleUsername(event) {
 		setUsername(event.target.value);
+		console.log(validator.username(event.target.value));
 	}
 
 	function handleEmail(event) {
 		setEmail(event.target.value);
-		// console.log(validator.email(email))
+		// console.log(validator.email(event.target.value))
 	}
 
 	function handlePassword(event) {
