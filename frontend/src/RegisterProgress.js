7484762210025203ffaeb132037e0bd0bd42c7e5
lenny-z <zhoulangchun@gmail.com>
2023-05-11 20:06:42 -0400

    Simplified, restyled registration progress bar

--- frontend/src/RegisterProgress.js
+++ frontend/src/RegisterProgress.js
@@ -1,20 +1,18 @@
 import { Link, useLocation } from 'react-router-dom';
 
-// const locationToField = new Map();
-// locationToField.set('/register/email', 'email');
-// locationToField.set('/register/username', 'username');
-// locationToField.set('/register/password', 'password');
-
 function RegisterProgressLink({ to, label, isValid }) {
 	const isActive = useLocation().pathname === to;
 
 	return (
-		<Link
-			to={to}
+		<li
 			className={isActive === true ? 'active' : ''}
 		>
-			{`${label} ${isValid === true ? '✅' : '❌'}`}
-		</Link>
+			<Link
+				to={to}
+			>
+				{`${label} ${isValid === true ? '✅' : '❌'}`}
+			</Link>
+		</li>
 	);
 }
 
@@ -23,45 +21,46 @@ export default function RegisterProgress({
 	usernameIsValid,
 	passwordIsValid,
 }) {
-	const path = useLocation().pathname;
-	var activeField = null;
+	// const path = useLocation().pathname;
+	// var activeField = null;
 
-	if (locationToField.has(path)) {
-		activeField = locationToField.get(path);
-	}
+	// if (locationToField.has(path)) {
+	// 	activeField = locationToField.get(path);
+	// }
 
-	function showValid(label, condition) {
-		return `${label} ${condition === true ? '✅' : '❌'}`;
-	}
+	// function showValid(label, condition) {
+	// 	return `${label} ${condition === true ? '✅' : '❌'}`;
+	// }
 
-	function isActiveField(field) {
-		return (activeField === field) ? 'active' : '';
-	}
+	// function isActiveField(field) {
+	// 	return (activeField === field) ? 'active' : '';
+	// }
 
 	return (
 		<nav id='register-progress-nav'>
-			{/* <Link
-				to='/register/email'
-				className={isActiveField('email')}
-			>
-				{showValid('Email', emailIsValid)}
-			</Link>
-			<Link
-				to='/register/username'
-				className={isActiveField('username')}
-			>
-				{showValid('Username', usernameIsValid)}
-			</Link>
-			<Link
-				to='/register/password'
-				className={isActiveField('password')}
-			>
-				{showValid('Password', passwordIsValid)}
-			</Link> */}
-			<RegisterProgressLink
-				to='/register/email'
-				label='Email'
-			/>
+			<ol>
+				{/* <li> */}
+				<RegisterProgressLink
+					to='/register/email'
+					label='Email'
+					isValid={emailIsValid}
+				/>
+				{/* </li> */}
+				{/* <li> */}
+				<RegisterProgressLink
+					to='/register/username'
+					label='Username'
+					isValid={usernameIsValid}
+				/>
+				{/* </li> */}
+				{/* <li> */}
+				<RegisterProgressLink
+					to='/register/password'
+					label='Password'
+					isValid={passwordIsValid}
+				/>
+				{/* </li> */}
+			</ol>
 		</nav>
 	);
 }
