fd9a18931228e35ea7f39fc52747486e39e28cb9
lenny-z <zhoulangchun@gmail.com>
2023-05-29 13:54:37 -0400

    Added client-side post validation

--- frontend/src/Editor.js
+++ frontend/src/Editor.js
@@ -1,12 +1,27 @@
 import { useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
 import axios from 'axios';
+import * as validator from '@lenny_zhou/validator';
+import Validations from './Validations.js';
 import './css/Editor.css';
 
 export default function Editor({ getAndShowPosts, parentPostID }) {
 	const [body, setBody] = useState('');
+	const pushAlert = useOutletContext()[0];
 
 	async function handleSubmit(event) {
 		event.preventDefault();
+		const validation = await validator.post(body);
+
+		if (!validator.allReqsMet(validation)) {
+			const alertBody = <>
+				<p>Post:</p>
+				<Validations reqsNotMet={validator.reqsNotMet(validation)} />
+			</>;
+
+			pushAlert(alertBody);
+			return;
+		}
 
 		const req = {
 			parentID: parentPostID,
